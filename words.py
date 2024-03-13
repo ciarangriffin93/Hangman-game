09ff2adbcbdd81b42c2ba89be31a188059abf55d
Ciaran Griffin <griffinciaran3@gmail.com>
2024-03-13 16:32:14 +0000

    Add words list

---
+++ words.py
@@ -0,0 +1,14 @@
+words_list = [
+    'ciaran',
+    'alan',
+    'david',
+    'robert',
+    'micheal',
+    'lisa',
+    'tim',
+    'kristyna',
+    'rory',
+    'paul',
+    'shauna',
+    'mairead',
+]
