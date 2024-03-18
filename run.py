08554bbb40fab26b184cd54e65d35d5e5c5a1237
Ciaran Griffin <griffinciaran3@gmail.com>
2024-03-18 14:40:31 +0000

    Change function for def hangman

--- run.py
+++ run.py
@@ -14,14 +14,13 @@ def get_valid_words(words_list):
     
     return word.upper()
 
-def play(word):
-    word_complettion = "_" * len(word)
-    guessed = false 
-    guessed_letters = []
-    guessed_words = []
-    tries = 6
-    print("let's play Hangman!")
-    print(display_hangman(tries))
-    print(word_complettion)
-    print("\n")
-    
+def hangman():
+
+    word =get_valid_words(words_list)
+    word_letter =set(words)
+    aplphabet =set(string.ascii_uppercase)
+    used_letter =set()
+
+    hangnan-stages = 6
+
+    while len(word_letter) > 0 and hangman_stages > 0:
